a791b5bdb8195c3fdf40939875707a931673f3d1
Jesse Cogollo <cogollo87@gmail.com>
2024-10-09 22:11:38 +0200

    first commit

---
+++ lib/the_plaza_elixir_challenge.ex
@@ -0,0 +1,18 @@
+defmodule ThePlazaElixirChallenge do
+  @moduledoc """
+  Documentation for `ThePlazaElixirChallenge`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ThePlazaElixirChallenge.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/the_plaza_elixir_challenge_test.exs
@@ -0,0 +1,8 @@
+defmodule ThePlazaElixirChallengeTest do
+  use ExUnit.Case
+  doctest ThePlazaElixirChallenge
+
+  test "greets the world" do
+    assert ThePlazaElixirChallenge.hello() == :world
+  end
+end
